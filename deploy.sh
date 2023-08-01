125d686231c6294ea853926288344f942b40f300
ElyasMoshirpanahi <elyasmoshirpanahe1376@gmail.com>
2023-08-01 23:00:04 +0330

    Removed deploy.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-export PYTHONPATH=$(pwd) &&
-pip install -r requirements.txt &&
-python src/cli.py download --output-file src/isnet/artifacts/model.pt &&
-python -m uvicorn deployment.main:app --host 0.0.0.0 --port 8000 
